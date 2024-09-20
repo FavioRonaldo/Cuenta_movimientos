a6d6fe9cebf3945f571cdfe1b58c41dbb0c03a29
FavioRonaldo <50154174+FavioRonaldo@users.noreply.github.com>
2024-09-20 17:30:34 -0500

    first commit

---
+++ java/com/challenge/Cuenta_movimientos/CuentaMovimientosApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.Cuenta_movimientos.Cuenta_movimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaMovimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaMovimientosApplication.class, args);
+	}
+
+}
---
+++ java/com/challenge/Cuenta_movimientos/model/entity/Movimiento.java
@@ -0,0 +1,10 @@
+package com.challenge.Cuenta_movimientos.model.entity;
+
+public class Movimiento {
+	private Integer numero_cuenta;
+	private String tipo;
+	private Float saldo_Inicial;
+	private Boolean estado;
+	private String movimiento;
+	
+}
