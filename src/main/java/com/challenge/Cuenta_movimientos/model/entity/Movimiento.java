08eaf59fe07071ee0b5f38895ffa97586625ddce
FavioRonaldo <50154174+FavioRonaldo@users.noreply.github.com>
2024-09-23 18:27:59 -0500

    fix movimietno

--- src/main/java/com/challenge/Cuenta_movimientos/model/entity/Movimiento.java
+++ src/main/java/com/challenge/Cuenta_movimientos/model/entity/Movimiento.java
@@ -12,9 +12,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
-
+@Getter
+@Setter
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
